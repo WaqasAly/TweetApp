2b2b2d54eb291745af3da68c621e5a696e45fbb6
Muhammad Waqas Rashid <102940015+WaqasAly@users.noreply.github.com>
2025-04-13 00:18:12 +0500

    Add registration and login templates

--- chaicenter/chaicenter/urls.py
+++ chaicenter/chaicenter/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.urls import include
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tweet.urls')),
     path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
--- chaicenter/tweet/forms.py
+++ chaicenter/tweet/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
-        # widgets = {
-        #     'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'What\'s happening?'})  # Placeholder for the text area
-        # }
-        # labels = {
-        #     'text': '',  # No label for the text area
-        #     'photo': 'Upload a photo'  # Label for the photo upload field
-        # }
+        
+class UserRegisterationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
